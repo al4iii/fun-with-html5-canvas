4a9997c29a820b1d1fa0a699979c94bb5c0189f0
Aliaksandr Danilin <al4icrux@gmail.com>
2020-12-01 11:47:21 +0300

    refactor: change structure of the project

--- main.js
+++ main.js
@@ -1,52 +1,37 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-ctx.strokeStyle = '#BADA55';
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
-ctx.lineWidth = 100;
-// ctx.globalCompositeOperation = 'multiply';
-
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
 let direction = true;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'miter';
+ctx.lineCap = 'round';
+ctx.lineWidth = 70;
 
 function draw(e) {
-  if (!isDrawing) return; // stop the fn from running when they are not moused down
-  console.log(e);
+  if (!isDrawing) return;
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-  ctx.beginPath();
-  // start from
+  ctx.beginPath();  
   ctx.moveTo(lastX, lastY);
-  // go to
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
-  [lastX, lastY] = [e.offsetX, e.offsetY];
-
+  [lastX, lastY] = [e.offsetX, e.offsetY];  
   hue++;
-  if (hue >= 360) {
-    hue = 0;
-  }
-  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-    direction = !direction;
-  }
-
-  if(direction) {
-    ctx.lineWidth++;
-  } else {
-    ctx.lineWidth--;
+  if (hue >= 360) 
+    hue = 0;  
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) 
+    direction = !direction;      
+  direction  ? ctx.lineWidth++ : ctx.lineWidth-- ;  
   }
 
-}
-
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
   [lastX, lastY] = [e.offsetX, e.offsetY];
 });
-
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', () => isDrawing = false);
-canvas.addEventListener('mouseout', () => isDrawing = false);
